ad80fc5f1510c008527404628402ab122cee6ebb
Saurav Rijal <sauravrijal1011@gmail.com>
2022-07-15 18:08:36 +0545

    Created Password Change view

--- Site/urls.py
+++ Site/urls.py
@@ -1,14 +1,17 @@
 from re import template
 from django.urls import URLPattern, path, include
 from . import views
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView
 
-from .views import UserLogin , Homepage, RegisterPage
+from .views import UserLogin , Homepage, RegisterPage, change_password
 
 urlpatterns = [
     path('', views.UserLogin.as_view(),name='login'),
     path('home', views.Homepage, name = 'home'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.RegisterPage.as_view(), name='register'),
+    path('password_change/', views.change_password, name='change_password'),
+    
+
     # path('login', views.tasklist, name = 'homepage'),
 ]
--- Site/views.py
+++ Site/views.py
@@ -4,6 +4,7 @@ from dataclasses import fields
 from pyexpat import model
 from re import template
 from django import forms
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
@@ -11,8 +12,10 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegistrationForm
 from django.views.generic.edit import FormView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import login
+from django.contrib.auth import update_session_auth_hash
+
 
 # from requests import request
 # from requests import request
@@ -92,6 +95,22 @@ class RegisterPage(FormView):
         if self.request.user.is_authenticated:
             return  redirect('home')
         return super(RegisterPage, self).get(*args, **kwargs)
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'Site/change_password.html', {
+        'form': form
+    })
 
 
 
