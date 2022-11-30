217d0d33232e7cab7075875afc0d860dafa5b5b1
Saurav <sauravrijal1011@gmail.com>
2022-11-30 20:10:11 +0545

    new branch

--- Site/models.py
+++ Site/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class account(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
 
